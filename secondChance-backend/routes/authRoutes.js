d9c5805a39f055e32f7decb2587cd2575a619194
vogiaan1904 <vogiaan1904@gmail.com>
2024-11-29 10:00:02 +0700

    update api

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -5,7 +5,10 @@ const bcryptjs = require("bcryptjs");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
-const logger = require("../logger");
+const { body, validationResult } = require("express-validator");
+const pino = require("pino");
+const logger = pino();
+
 router.post("/register", async (req, res, next) => {
   try {
     const db = await connectToDatabase();
@@ -74,6 +77,58 @@ router.post("/login", async (req, res, next) => {
     const authtoken = jwt.sign(payload, JWT_SECRET);
     logger.info("User logged in successfully");
     return res.status(200).json({ authtoken, userName, userEmail });
+  } catch (e) {
+    next(e);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+router.put("/update", async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      logger.error("Validation errors in update request", errors.array());
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const email = req.headers.email;
+
+    if (!email) {
+      logger.error("Email not found in the request headers");
+      return res
+        .status(400)
+        .json({ error: "Email not found in the request headers" });
+    }
+
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+
+    const existingUser = await collection.findOne({ email });
+
+    if (!existingUser) {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    existingUser.firstName = req.body.name;
+    existingUser.updatedAt = new Date();
+
+    const updatedUser = await collection.findOneAndUpdate(
+      { email },
+      { $set: existingUser },
+      { returnDocument: "after" }
+    );
+
+    const payload = {
+      user: {
+        id: updatedUser._id.toString(),
+      },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    logger.info("User updated successfully");
+    return res.status(200).json({ authtoken });
   } catch (e) {
     next(e);
     return res.status(500).send("Internal server error");
